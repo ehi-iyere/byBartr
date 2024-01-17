f42a15ce8a71329dc70fd3cf93a02362104b8524
ehi-iyere <ebehii47@gmail.com>
2024-01-17 16:07:50 -0500

    Creating pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/Logout.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export default function Logout() {
+  return <div>Logout</div>;
+}
