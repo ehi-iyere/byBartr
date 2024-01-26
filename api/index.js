c8946a92636879eb4fc2fac9da791612235e85a9
ehi-iyere <ebehii47@gmail.com>
2024-01-26 18:01:54 -0500

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config();
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((error) => {
+    console.log("error");
+  });
 const app = express();
 
 app.listen(3000, () => {
