26c265549b1a2c6867c6f59e68239059be8f0a6e
ehi-iyere <ebehii47@gmail.com>
2024-01-26 21:33:09 -0500

    auth routes and controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,3 +4,4 @@ export const test = (req, res) => {
   });
 };
 //Testing API routes
+// return a message saying the "Testing APU routes
--- api/index.js
+++ api/index.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { error } from "console";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 mongoose
-  .connect(process.env.Mongo)
+  .connect(process.env.Mongo, {
+    autoIndex: true, //make this also true
+  })
   .then(() => {
     console.log("connected to db");
   })
@@ -13,9 +16,10 @@ mongoose
     console.log("error");
   });
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
