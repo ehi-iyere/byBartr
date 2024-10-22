3ae2427ccf9b75e5f05ea5313bb09388f2e82a4e
ehi-iyere <ebehii47@gmail.com>
2024-10-22 00:11:18 -0400

    edited the project by id route

--- server/routes/project.js
+++ server/routes/project.js
@@ -6,7 +6,7 @@ const projectController = require("../controllers/project-controller");
 
 router.post("/createproject", authorize, projectController.createProject);
 router.get("/", projectController.getProjects);
-router.get("/project/:projectId", projectController.getProjectbyId);
+router.get("/:projectId/details", projectController.getProjectbyId);
 router.get("/:user_id/projects", projectController.getProjectByUser);
 router.get("/theme", projectController.getAllThemes);
 router.put("/edit/:projectId", authorize, projectController.editProject);
